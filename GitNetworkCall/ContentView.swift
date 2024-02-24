8724812656838f2586b8cde591455fa366f90bd9
RAJPARA DHRUV <dhruvrajpara0111@gmail.com>
2024-02-24 14:31:33 -0500

    Configure Project

---
+++ GitNetworkCall/ContentView.swift
@@ -0,0 +1,29 @@
+//
+//  ContentView.swift
+//  GitNetworkCall
+//
+//  Created by RAJPARA DHRUV on 2024-02-24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+                .frame(width: 53, height: 53)
+            Text("Hello, world!")
+                .bold()
+                .font(.system(size: 30))
+            Text("App will Show data here.The data will be fetched from Github")
+                .padding()
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GitNetworkCall/GitNetworkCallApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitNetworkCallApp.swift
+//  GitNetworkCall
+//
+//  Created by RAJPARA DHRUV on 2024-02-24.
+//
+
+import SwiftUI
+
+@main
+struct GitNetworkCallApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
