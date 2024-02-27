269efb0df74dac8a270433bb30d11ef38615f61a
RAJPARA DHRUV <dhruvrajpara0111@gmail.com>
2024-02-26 21:52:57 -0500

    add loader and code refactor

--- GitNetworkCall/UserDetailView.swift
+++ GitNetworkCall/UserDetailView.swift
@@ -10,14 +10,19 @@ import SwiftUI
 struct UserDetailView: View {
 
     @StateObject var viewModel = UserDetailViewModel()
-
+    @State var username: String = ""
+   
+    
     var body: some View {
         VStack {
             AsyncImage(url: URL(string: viewModel.user?.avatarUrl ?? "")) { image in
                 image
-                    .image?.resizable()
+                    .resizable()
                     .aspectRatio(contentMode: .fit)
                     .clipShape(.circle)
+            } placeholder: {
+                Circle()
+                    .foregroundColor(.gray)
             }
             .frame(width: 73, height: 73)
             Text(viewModel.user?.login ?? "")
@@ -25,10 +30,45 @@ struct UserDetailView: View {
                 .font(.system(size: 30))
             Text(viewModel.user?.bio ?? "")
                 .padding()
+
+            
+            TextField("Username", text: $username)
+                .border(.black, width: 1)
+                .frame(height: 23)
+                .padding()
+            
+            Button(action: {
+                viewModel.isLoading = true
+                viewModel.viewDidAppear(username: username)
+               
+            }) {
+                Text("Get Data")
+                    .padding()
+            }
+            .background(Color.blue)
+            .frame(maxWidth: .infinity)
+            .foregroundColor(.white)
+            .cornerRadius(23)
         }
-        .padding()
         .onAppear {
-            viewModel.viewDidAppear()
+            viewModel.viewDidAppear(username: "twostraws")
+        }
+        .overlay(loaderView())
+    }
+    
+    @ViewBuilder
+    private func loaderView() -> some View {
+        if viewModel.isLoading {
+            ZStack {
+                ProgressView(label: {
+                    Text("Loading..")
+                        .font(.system(size: 20))
+                        .frame(width: 100)
+                })
+                .tint(.blue)
+                .frame(width: 150, height: 100)
+                .background(.thinMaterial)
+            }
         }
     }
 }
--- GitNetworkCall/UserDetailViewModel.swift
+++ GitNetworkCall/UserDetailViewModel.swift
@@ -10,26 +10,32 @@ import Foundation
 @MainActor class UserDetailViewModel: ObservableObject {
 
     @Published var user: User?
+    @Published var isLoading: Bool = true
     
     init(user: User? = nil) {
         self.user = user
     }
 
-    func viewDidAppear() {
+    func viewDidAppear(username: String) {
         Task {
             do {
-                user = try await getUser()
+                try await Task.sleep(nanoseconds: 4000000000)
+                user = try await getUser(username: username)
+                isLoading = false
             } catch GithubError.invalidData {
+                isLoading = false
                 print("Data is Invalid")
             } catch GithubError.invalidResponse {
+                isLoading = false
                 print("Response is Invalid")
             } catch GithubError.invalidURL {
+                isLoading = false
                 print("URL is Invalid")
             }
         }
     }
-    private func getUser() async throws -> User {
-        let endPoint = "https://api.github.com/users/rajparad"
+    private func getUser(username: String) async throws -> User {
+        let endPoint = "https://api.github.com/users/\(username)"
         
         guard let url = URL(string: endPoint) else {
             throw GithubError.invalidURL
